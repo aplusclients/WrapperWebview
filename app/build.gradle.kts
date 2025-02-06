09cd8fcad63ae118439d63e5b0dea89404b00fca
Minimalist Tech <minimalisttech@icloud.com>
2025-02-06 10:25:19 -0500

    Add themed icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "1.5"
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
